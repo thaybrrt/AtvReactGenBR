aaa7b0de6413d759405db8a896bcbb93305b6a60
thaybrrt <thaybarreto@live.com>
2024-02-14 13:37:21 -0300

    primeiro commit

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home () {
+    return (
+        <>
+            <h1 className="titulo">Blink-182</h1>
+            <img className="img"src="https://media.moneytimes.com.br/uploads/2023/11/blink182.jpg" alt="Imagem da banda Blink-182" />
+        </>
+   )
+}
+
+export default Home;
